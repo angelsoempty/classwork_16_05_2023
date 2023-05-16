067b671e1f7061f979e2a86fa964a19bbc3137b3
angelsoempty <denatamanov1@gmail.com>
2023-05-16 20:43:35 +0300

    #1

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
+class InvalidUsernameError(Exception):
+    def __init__(self, username):
+        self.username = username
+        super().__init__('Invalid username')
+
+#InvalidLengthError
+#InvalidCharacterError
+#DublicateUsernameError
+
+def register_user(username):
+    if len(username) < 5:
+        raise InvalidUsernameError(username)
+
 try:
-    print('start code')
-    print(error)
-    print('end')
-except:
-    print('no problems')
-print('any code...')
-
-def checker(var_1):
-    if type(var_1) != str:
-        raise TypeError(f'Sorry, we cant work with {type(var_1)}, we need class str')
-    else:
-        return var_1
-a = '123'
-checker(a)
-
-class BuildingHouseError(Exception):
-    def __str__(self):
-        return 'щось не те, дуже багато і дорого'
-
-def check_material_build(amount, limit):
-    if amount > limit:
-        return 'Достатньо матеріалів!'
-    else:
-        raise BuildingHouseError()
-print(check_material_build(9,10))
+    username = input("Введіть ім'я користувача: ")
+    register_user(username)
+    print('Реєстрація успішна')
+except InvalidUsernameError as e:
+    print('Помилка.')
