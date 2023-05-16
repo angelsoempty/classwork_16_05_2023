99c4db5e4db09757cb1e328c5125f3c570bbddd2
angelsoempty <denatamanov1@gmail.com>
2023-05-16 20:06:54 +0300

    використання власних винятків

--- main.py
+++ main.py
@@ -13,3 +13,14 @@ def checker(var_1):
         return var_1
 a = '123'
 checker(a)
+
+class BuildingHouseError(Exception):
+    def __str__(self):
+        return 'щось не те, дуже багато і дорого'
+
+def check_material_build(amount, limit):
+    if amount > limit:
+        return 'Достатньо матеріалів!'
+    else:
+        raise BuildingHouseError()
+print(check_material_build(9,10))
