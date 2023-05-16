7100c52c959d77997599f9a644e8030ec9318152
angelsoempty <denatamanov1@gmail.com>
2023-05-16 19:18:34 +0300

    використання власних винятків

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ try:
 except:
     print('no problems')
 print('any code...')
+
+def checker(var_1):
+    if type(var_1) != str:
+        raise TypeError(f'Sorry, we cant work with {type(var_1)}, we need class str')
+    else:
+        return var_1
+a = '123'
+checker(a)
